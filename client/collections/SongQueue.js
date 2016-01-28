b969bd1fc67455116a9aef6109492a61e6600b16
danmolitor <danmolitor91@gmail.com>
2016-01-27 16:57:10 -0800

    SongQueue.js specs passed

---
+++ client/collections/SongQueue.js
@@ -0,0 +1,35 @@
+// SongQueue.js - Defines a backbone model class for the song queue.
+var SongQueue = Songs.extend({
+
+    //model: SongModel
+    //play: function(){
+    //this.trigger('play', this);
+    //}
+    initialize: function() {
+
+        this.on('add', function() {
+            if (this.length < 2) {
+                this.playFirst();
+            }
+        }, this);
+
+        this.on('ended', function(){
+          this.remove(this.at(0));
+          if (this.length > 0){
+            this.playFirst();
+          }
+        }, this);
+
+        this.on('dequeue', function(){
+          this.remove(this.at(0));
+        }, this);
+
+    },
+
+    playFirst: function() {
+        this.at(0).play();
+    },
+
+});
+
+// collection.remove(models, [options])
